c8351b9dc7db18d1aa3fcc74430603175670ba7f
TonyDonDon <70017801+TonyDonDon@users.noreply.github.com>
2021-08-14 11:29:41 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'c27d5cb77c2f4aa8ae6eb5a5d09de701',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '職女Jennifer',
+  domain: 'https://jennifer-b.notion.site/Jennifer-c27d5cb77c2f4aa8ae6eb5a5d09de701',
+  author: 'Jennifer',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  description: '在Podcast閒聊生活，在文字裡討論文學。',
+  socialImageTitle: '職女Jennifer',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  matters: '職女Jennifer',
+  instagram: '第22條',
+  podcast: '第22條',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
